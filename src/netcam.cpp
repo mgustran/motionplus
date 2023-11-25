3297bf4ac14e93ac4a760a4b8dadf66f8f51f295
mgustran <mgustran@gmail.com>
2023-11-25 23:57:03 +0100

    fix https url parsing

--- src/netcam.cpp
+++ src/netcam.cpp
@@ -217,7 +217,9 @@ static void netcam_url_parse(ctx_url *parse_url, const char *text_url)
     }
     if (((!parse_url->port) && (parse_url->service)) ||
         ((parse_url->port > 65535) && (parse_url->service))) {
-        if (mystreq(parse_url->service, "http")) {
+        if (mystreq(parse_url->service, "https")) {
+            parse_url->port = 443;
+        } else if (mystreq(parse_url->service, "http")) {
             parse_url->port = 80;
         } else if (mystreq(parse_url->service, "ftp")) {
             parse_url->port = 21;
